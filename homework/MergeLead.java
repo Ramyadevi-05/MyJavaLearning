39ac86eac9b259555e23ac9562a42fc3cb6460e1
Ramyadevi-05 <82906795+Ramyadevi-05@users.noreply.github.com>
2021-05-15 23:23:41 +0530

    Add files via upload
    Committing all the leaf taps exercise code

---
+++ homework/MergeLead.java
@@ -0,0 +1,59 @@
+package week5.day1.homework;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class MergeLead extends BaseTest{
+	@Test
+	public void mergelead() throws InterruptedException {
+		
+		//To click on the Merge Leads link
+		driver.findElementByXPath("//a[text()='Merge Leads']").click();
+		
+		//To click on the From Lead 	
+		driver.findElementByXPath("(//img[@alt='Lookup'])[1]").click();
+		
+		//To search with First Name
+		driver.findElementByXPath("//input[@name='firstName']").sendKeys("Ramyadevi");
+		
+		//To click on find leads		
+		driver.findElementByXPath("//button[text()='Find Leads']").click();
+		Thread.sleep(2000);
+		
+		//Click on first resulting lead		
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a[1]")).click();
+		Thread.sleep(2000);
+		
+		//To click on the To Lead 
+		driver.findElementByXPath("(//img[@alt='Lookup'])[2]").click();
+		
+		//To click on find leads		
+		driver.findElementByXPath("//button[text()='Find Leads']").click();
+		
+		//Click on first resulting lead		
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a[2]")).click();
+		Thread.sleep(2000);
+		
+		//Click on Merge Lead
+		driver.findElement(By.xpath("//a[text()='Merge']")).click();
+		
+		//Clicking on Alert 
+		driver.switchTo().alert().accept();
+		
+		String Title=driver.getTitle();
+		
+		System.out.println(Title);
+		
+		if(Title.equalsIgnoreCase("View Lead | opentaps CRM"))
+		{
+			System.out.println("Title is matched");
+		}
+		else
+		{
+			System.out.println("Title is not matched");
+		}
+		
+		
+	}
+
+}
